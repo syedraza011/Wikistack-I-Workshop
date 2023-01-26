a5c586e77ea7ad3ebb3fbd0ec57157ee96a3fe14
Syed Raza <syed.raza011@gmail.com>
2023-01-26 17:56:40 -0500

    database and server

--- app.js
+++ app.js
@@ -3,14 +3,29 @@ const app=express();
 const morgan=require('morgan')
 app.use(morgan('div'))
 const html=require ('html-template-tag')
-app.use(express.static("assests"));
+app.use(express.static("./assests"));
 app.use(express.urlencoded({ extended: false }));
 const { addPage, editPage, main, userList, userPages, wikiPage } = require('./views');
-
-
+const { db, user, page } = require('./models');
+ 
+db.authenticate() 
+  .then(() => { 
+    console.log('connected to the database'); 
+})
+//creating tables user and pages
+const init= async()=>{
+  const initialization = await db.sync()
+return initialization;
+  
+}
+init();
 app.get('/',(req,res)=>{
   res.send("hello world");  
 });
+
+
+
+
 
 const PORT=3000;
 app.listen(PORT,()=>{
